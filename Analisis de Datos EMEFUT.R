0e82c69369fd13a1c7cf26572c2d512da61241cf
Andres Cifuentes <andrescif@gmail.com>
2020-06-28 21:29:31 -0600

    Se han cambiado los nombres de la base de datos para utilizar variables.  Además, se han corregido los errores detectados en la base de datos.

--- Analisis de Datos EMEFUT.R
+++ Analisis de Datos EMEFUT.R
@@ -7,7 +7,19 @@ Avances.hasta.domingo.28.de.junio <-
 Datos.profesores_categorias.EMEFUT <- 
   read.csv("~/R Projects/Reportes_EMEFUT/Datos/Datos profesores_categorias EMEFUT.csv")
 
+#Cambiar nombre para utilizar variables
+datos <- Avances.hasta.domingo.28.de.junio
 
+names(datos) <-  c("Fecha de reporte","Fecha de contacto",
+                   "Sede","Categoría","¿Llamadas?",
+                   "Llamadas realizadas","Llamadas efectivas",
+                   "¿Mensajes?", "Mensajes enviados",
+                   "Mensajes efectivos","¿Entrenamientos?",
+                   "¿Cuántos entrenamientos?", "%Recibió",
+                   "%Realizó","Comentario")
+
+names(Datos.profesores_categorias.EMEFUT) <- c("Fecha de reporte","Nombre del Profesor",
+                                               "Sede","Categoría","NIT")
 
 
 
