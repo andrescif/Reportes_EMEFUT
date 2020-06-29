0219264e52317240e0710b021e02468c50b5bcd0
Andres Cifuentes <andrescif@gmail.com>
2020-06-28 19:54:39 -0600

    Se han cargado todos los datos y se han colocado nombres a las columnas.  Se cuenta con los datos en brutos de contactos y los de profesores.

---
+++ Analisis de Datos EMEFUT.R
@@ -0,0 +1,16 @@
+library(haven)
+library(tidyverse)
+
+#Carcgar datos
+Avances.hasta.domingo.28.de.junio <- 
+  read.csv("~/R Projects/Reportes_EMEFUT/Datos/Avances hasta domingo 28 de junio.csv")
+Datos.profesores_categorias.EMEFUT <- 
+  read.csv("~/R Projects/Reportes_EMEFUT/Datos/Datos profesores_categorias EMEFUT.csv")
+
+
+
+
+
+
+
+
