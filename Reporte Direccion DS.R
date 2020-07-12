1aa151dbed0812ade70ebf4d09b1c44feb071583
Andres Cifuentes <andrescif@gmail.com>
2020-07-12 12:54:53 -0600

    Se han cargado los datos del domingo 12 de julio y se ha iniciado con el desarrollo de un reporte para la direccion.

---
+++ Reporte Direccion DS.R
@@ -0,0 +1,19 @@
+library(haven)
+library(tidyverse)
+
+#Cargar datos
+Avances.hasta.domingo.12.de.julio <- 
+  read.csv("~/R Projects/Reportes_EMEFUT/Datos/Avances hasta domingo 12 de julio.csv")
+
+
+
+
+
+
+
+
+
+
+
+
+
