5447049367189c165e04811d3483c1aa1501dcca
Andres Cifuentes <andrescif@gmail.com>
2020-07-12 12:59:45 -0600

    Limpiados los datos y listos para los análisis.

--- Reporte Direccion DS.R
+++ Reporte Direccion DS.R
@@ -5,8 +5,17 @@ library(tidyverse)
 Avances.hasta.domingo.12.de.julio <- 
   read.csv("~/R Projects/Reportes_EMEFUT/Datos/Avances hasta domingo 12 de julio.csv")
 
+#Cambiar nombre para utilizar variables
+datos <- Avances.hasta.domingo.12.de.julio
 
-
+names(datos) <-  c("Fecha de reporte","Fecha de contacto",
+                   "Sede","Categoría","¿Llamadas?",
+                   "Llamadas realizadas","Llamadas efectivas",
+                   "¿Mensajes?", "Mensajes enviados",
+                   "Mensajes efectivos","¿Entrenamientos?",
+                   "¿Cuántos entrenamientos?", "%Recibió",
+                   "%Realizó","Comentario")
+rm(Avances.hasta.domingo.12.de.julio)
 
 
 
